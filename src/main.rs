8983aadc5ee1a97032235410d6ebac64ad4a2f8a
Joe Grund <jgrund@whamcloud.io>
2018-08-20 10:23:28 -0400

    Add stats parser
    Add a parser for collecting the stats file.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 #[macro_use]
 extern crate serde_derive;
 
@@ -8,7 +12,11 @@ extern crate combine;
 use combine::Parser;
 use std::str;
 
+mod base_parsers;
 mod brw_stats_parser;
+mod snapshot_time;
+mod stats_parser;
+
 use brw_stats_parser::brw_stats;
 
 use std::process::Command;
---
+++ src/snapshot_time.rs
@@ -0,0 +1,75 @@
+// Copyright (c) 2018 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+use base_parsers::{digits, till_newline};
+use combine::error::ParseError;
+use combine::parser::char::{spaces, string};
+use combine::stream::Stream;
+use combine::{optional, token, Parser};
+
+pub fn snapshot_time<I>() -> impl Parser<Input = I, Output = String>
+where
+    I: Stream<Item = char>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    (
+        string("snapshot_time").skip(optional(token(':'))),
+        spaces(),
+        digits().skip(token('.')),
+        digits().skip(till_newline()),
+    ).map(|(_, _, secs, nsecs)| format!("{}.{}", secs, nsecs))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use combine::stream::state::{SourcePosition, State};
+
+    #[test]
+    fn test_snapshot_time() {
+        let x = State::new(
+            r#"snapshot_time:         1534158712.738772898 (secs.nsecs)
+"#,
+        );
+
+        let result = snapshot_time().easy_parse(x);
+
+        assert_eq!(
+            result,
+            Ok((
+                "1534158712.738772898".to_string(),
+                State {
+                    input: "\n",
+                    positioner: SourcePosition {
+                        line: 1,
+                        column: 57
+                    }
+                }
+            ))
+        );
+    }
+    #[test]
+    fn test_snapshot_time_no_colon() {
+        let x = State::new(
+            r#"snapshot_time             1534769431.137892896 secs.nsecs
+"#,
+        );
+
+        let result = snapshot_time().easy_parse(x);
+
+        assert_eq!(
+            result,
+            Ok((
+                "1534769431.137892896".to_string(),
+                State {
+                    input: "\n",
+                    positioner: SourcePosition {
+                        line: 1,
+                        column: 58
+                    }
+                }
+            ))
+        );
+    }
+}
