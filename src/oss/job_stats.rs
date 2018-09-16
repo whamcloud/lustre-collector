a6a223e8595decc26ad3735d2270a8f16caac321
Joe Grund <jgrund@whamcloud.io>
2018-09-16 00:54:07 -0400

    remove commented code.
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- src/oss/job_stats.rs
+++ src/oss/job_stats.rs
@@ -17,26 +17,6 @@ use serde_yaml;
 
 use types::{JobStatOst, JobStatsOst};
 
-/*
-obdfilter.fs-OST0001.job_stats=job_stats:
-obdfilter.fs-OST0003.job_stats=
-job_stats:
-- job_id:          cp.0
-  snapshot_time:   1537063675
-  read_bytes:      { samples:         256, unit: bytes, min: 4194304, max: 4194304, sum:      1073741824 }
-  write_bytes:     { samples:         256, unit: bytes, min: 4194304, max: 4194304, sum:      1073741824 }
-  getattr:         { samples:           0, unit:  reqs }
-  setattr:         { samples:           0, unit:  reqs }
-  punch:           { samples:           0, unit:  reqs }
-  sync:            { samples:           0, unit:  reqs }
-  destroy:         { samples:           0, unit:  reqs }
-  create:          { samples:           0, unit:  reqs }
-  statfs:          { samples:           0, unit:  reqs }
-  get_info:        { samples:           0, unit:  reqs }
-  set_info:        { samples:           0, unit:  reqs }
-  quotactl:        { samples:           0, unit:  reqs }
-*/
-
 pub fn parse<I>() -> impl Parser<Input = I, Output = Option<Vec<JobStatOst>>>
 where
     I: Stream<Item = char>,
