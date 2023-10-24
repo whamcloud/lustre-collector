ecdee16962fa759a8a7a8b11b5458df39705794f
Raphael Druon <rdruon@ddn.com>
2023-10-24 03:59:05 -0600

    Fix clippy

--- src/mgs/mgs_fs_parser.rs
+++ src/mgs/mgs_fs_parser.rs
@@ -75,7 +75,7 @@ where
                 let mgs_map: HashMap<Target, Vec<FsName>> =
                     xs.into_iter()
                         .fold(HashMap::new(), |mut acc, (target, fs_name)| {
-                            let fs_names = acc.entry(target.clone()).or_insert_with(Vec::new);
+                            let fs_names = acc.entry(target.clone()).or_default();
                             let names: Vec<FsName> = [&fs_names[..], &vec![fs_name][..]].concat();
                             acc.insert(target, names);
 
