b7eabe76ecfde9d11b4544c71e7522fb90078336
Andy Breuhan <abreuhan@ddn.com>
2024-05-08 16:15:06 +0200

    Removed non existent mdt.* stats. These are part of the OSD parser. As seen for example here: osd-ldiskfs.ai400x2-MDT0000.filestotal=97714176 Therefore the test coverage didn't detected them.

--- src/mds/mdt_parser.rs
+++ src/mds/mdt_parser.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 DDN. All rights reserved.
+// Copyright (c) 2024 DDN. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
@@ -22,26 +22,11 @@ use combine::{
 pub(crate) const JOB_STATS: &str = "job_stats";
 pub(crate) const STATS: &str = "md_stats";
 pub(crate) const NUM_EXPORTS: &str = "num_exports";
-pub(crate) const FILES_FREE: &str = "filesfree";
-pub(crate) const FILES_TOTAL: &str = "filestotal";
-pub(crate) const KBYTES_AVAIL: &str = "kbytesavail";
-pub(crate) const KBYTES_FREE: &str = "kbytesfree";
-pub(crate) const KBYTES_TOTAL: &str = "kbytestotal";
 
 enum MdtStat {
     JobStats(Option<Vec<JobStatMdt>>),
     Stats(Vec<Stat>),
     NumExports(u64),
-    /// Available inodes
-    FilesFree(u64),
-    /// Total inodes
-    FilesTotal(u64),
-    /// Available disk space
-    BytesAvail(u64),
-    /// Free disk space
-    BytesFree(u64),
-    /// Total disk space
-    BytesTotal(u64),
     ExportStats(Vec<ExportStats>),
 }
 
@@ -58,35 +43,6 @@ where
         (param(STATS), stats().map(MdtStat::Stats)).message("while parsing mdt_stat"),
         (param(JOB_STATS), job_stats::parse().map(MdtStat::JobStats))
             .message("while parsing job_stats"),
-        (
-            param(FILES_FREE),
-            digits().skip(newline()).map(MdtStat::FilesFree),
-        ),
-        (
-            param(FILES_TOTAL),
-            digits().skip(newline()).map(MdtStat::FilesTotal),
-        ),
-        (
-            param(KBYTES_AVAIL),
-            digits()
-                .skip(newline())
-                .map(|x| x * 1024)
-                .map(MdtStat::BytesAvail),
-        ),
-        (
-            param(KBYTES_FREE),
-            digits()
-                .skip(newline())
-                .map(|x| x * 1024)
-                .map(MdtStat::BytesFree),
-        ),
-        (
-            param(KBYTES_TOTAL),
-            digits()
-                .skip(newline())
-                .map(|x| x * 1024)
-                .map(MdtStat::BytesTotal),
-        ),
         (
             param_period(EXPORTS),
             exports_stats().map(MdtStat::ExportStats),
@@ -138,36 +94,6 @@ where
                 value,
             }),
             MdtStat::NumExports(value) => TargetStats::NumExports(TargetStat {
-                kind: TargetVariant::Mdt,
-                target,
-                param,
-                value,
-            }),
-            MdtStat::FilesFree(value) => TargetStats::FilesFree(TargetStat {
-                kind: TargetVariant::Mdt,
-                target,
-                param,
-                value,
-            }),
-            MdtStat::FilesTotal(value) => TargetStats::FilesTotal(TargetStat {
-                kind: TargetVariant::Mdt,
-                target,
-                param,
-                value,
-            }),
-            MdtStat::BytesAvail(value) => TargetStats::KBytesAvail(TargetStat {
-                kind: TargetVariant::Mdt,
-                target,
-                param,
-                value,
-            }),
-            MdtStat::BytesFree(value) => TargetStats::KBytesFree(TargetStat {
-                kind: TargetVariant::Mdt,
-                target,
-                param,
-                value,
-            }),
-            MdtStat::BytesTotal(value) => TargetStats::KBytesTotal(TargetStat {
                 kind: TargetVariant::Mdt,
                 target,
                 param,
